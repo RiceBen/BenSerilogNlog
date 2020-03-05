c6647bbb7cc84ae23a817d18ec873c842956e6f6
benjamin <benjamin750715@gmail.com>
2020-03-05 20:32:21 +0800

    Re-init project

--- BenSerilogNlog/BenSerilogNlog/MyLogger.cs
+++
@@ -1,29 +0,0 @@
-﻿using System;
-using Serilog;
-
-namespace BenSerilogNlog
-{
-    public class MyLogger
-    {
-        /// <summary>
-        /// The logger.
-        /// </summary>
-        private ILogger _logger;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="T:BenSerilogNlog.MyLogger"/> class.
-        /// </summary>
-        public MyLogger()
-        {
-            this._logger = new LoggerConfiguration()                 .WriteTo.Console(                     outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss} [{Level}] {Message}{NewLine}{Exception}"                 )                 .CreateLogger();
-        }
-
-        /// <summary>
-        /// Logs the info.
-        /// </summary>
-        /// <param name="message">Message.</param>
-        public void LogInfo(string message){
-            this._logger.Information(message);
-        }
-    }
-}
