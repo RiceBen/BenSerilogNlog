00f79bde03318c4a78ecc3aebf41e8b75a59f155
benjamin <benjamin750715@gmail.com>
2020-03-22 17:42:26 +0800

    Refactor LogFactory

--- BenSerilogNlog/LogFactory.cs
+++ BenSerilogNlog/LogFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Serilog;
+
 using Serilog.Filters;
 
 namespace BenSerilogNlog
@@ -7,28 +8,53 @@ namespace BenSerilogNlog
     {
         public static void InitMyLogger()
         {
-            var isBSLN = Matching.FromSource<BusinessLogic>();
-            var expr = "@Level = 'Information' and AppId3 is not null";
 
             Log.Logger = new LoggerConfiguration()
                             .Enrich.WithProperty("AppId", new { Id = 1, Name = "Ben" })
-                            .WriteTo.Console(
-                                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
-                                outputTemplate: "{Timestamp:HH:mm:ss} [AppId:{AppId} {Level:u3}] {Message:lj}{NewLine}{Exception}")
-                            .WriteTo.NLog(Serilog.Events.LogEventLevel.Error, "[AppId:{AppId}] {Message:lj}{NewLine}{Exception}")
-                            .WriteTo.Logger(logger =>
-                                logger.Enrich.WithProperty("AppId2", 10)
-                                      .Filter
-                                      .ByIncludingOnly(isBSLN)
-                                      .WriteTo.Console(Serilog.Events.LogEventLevel.Warning, "{Timestamp:HH:mm:ss} [AppId:{AppId2}{AppId} {Level:u3}]$ {Message:lj}{NewLine}{Exception}"))
-                            .WriteTo.Logger(logger=>
-                                logger.Enrich.WithProperty("AppId3", "[Terra]")
-                                      .Filter
-                                      .ByIncludingOnly(expr)
-                                      .WriteTo.Console(
-                                        restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
-                                        outputTemplate: "{Timestamp:HH:mm:ss} [AppId:{AppId3}{AppId2}{AppId} {Level:u3}]$ {Message:lj}{NewLine}{Exception}"))
+                            .WriteTo.Logger(MyDefaultConsoleSinkConfiger)
+                            .WriteTo.Logger(MyDefaultNLogSinkConfiger)
+                            .WriteTo.Logger(ConsoleLogWithPropAndFilterWithClassSinkConfiger)
+                            .WriteTo.Logger(ConsoleLogWithPropAndFilterWithRegexSinkConfiger)
                             .CreateLogger();
+        }
+
+        private static void MyDefaultNLogSinkConfiger(LoggerConfiguration lscf)
+        {
+            lscf.WriteTo
+                .NLog(Serilog.Events.LogEventLevel.Error, "[AppId:{AppId}] {Message:lj}{NewLine}{Exception}");
+        }
+
+        private static void MyDefaultConsoleSinkConfiger(LoggerConfiguration lscf)
+        {
+            lscf.WriteTo.Console(
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                outputTemplate: "{Timestamp:HH:mm:ss} [AppId:{AppId} {Level:u3}] {Message:lj}{NewLine}{Exception}");
+        }
+
+        private static void ConsoleLogWithPropAndFilterWithClassSinkConfiger(LoggerConfiguration lscf)
+        {
+            var isBSLN = Matching.FromSource<BusinessLogic>();
+
+            lscf.Enrich
+                .WithProperty("AppId2", 10)
+                .Filter
+                .ByIncludingOnly(isBSLN)
+                .WriteTo.Console(
+                    Serilog.Events.LogEventLevel.Warning,
+                    "{Timestamp:HH:mm:ss} [AppId:{AppId2}{AppId} {Level:u3}]$ {Message:lj}{NewLine}{Exception}");
+        }
+
+        private static void ConsoleLogWithPropAndFilterWithRegexSinkConfiger(LoggerConfiguration lscf)
+        {
+            var expr = "@Level = 'Information' and AppId3 is not null";
+
+            lscf.Enrich
+                .WithProperty("AppId3", "[MyService]")
+                .Filter
+                .ByIncludingOnly(expr)
+                .WriteTo.Console(
+                    restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
+                    outputTemplate: "{Timestamp:HH:mm:ss} [AppId:{AppId3}{AppId2}{AppId} {Level:u3}]$ {Message:lj}{NewLine}{Exception}");
         }
     }
 }
