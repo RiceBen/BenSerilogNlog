9bb6e6c0ea440e8c733ad191d7440b4ae5b21eed
benjamin <benjamin750715@gmail.com>
2020-04-07 19:58:09 +0800

    Add advance example
    refactor LoggerConfiguration
    add AwsCloudwatch sink

--- BenSerilogNlog/LogFactory.cs
+++ BenSerilogNlog/LogFactory.cs
@@ -1,6 +1,10 @@
-﻿using Serilog;
-
+﻿using System;
+using Amazon;
+using Amazon.CloudWatchLogs;
+using Serilog;
 using Serilog.Filters;
+using Serilog.Formatting;
+using Serilog.Sinks.AwsCloudWatch;
 
 namespace BenSerilogNlog
 {
@@ -15,6 +19,7 @@ namespace BenSerilogNlog
                             .WriteTo.Logger(MyDefaultNLogSinkConfiger)
                             .WriteTo.Logger(ConsoleLogWithPropAndFilterWithClassSinkConfiger)
                             .WriteTo.Logger(ConsoleLogWithPropAndFilterWithRegexSinkConfiger)
+                            .WriteTo.Logger(AwsCloudwatchSinkConfiger)
                             .CreateLogger();
         }
 
@@ -55,6 +60,36 @@ namespace BenSerilogNlog
                 .WriteTo.Console(
                     restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information,
                     outputTemplate: "{Timestamp:HH:mm:ss} [AppId:{AppId3}{AppId2}{AppId} {Level:u3}]$ {Message:lj}{NewLine}{Exception}");
+        }
+
+        private static void AwsCloudwatchSinkConfiger(LoggerConfiguration lscf)
+        {
+            // name of the log group
+            var logGroupName = "myLogGroup/dev";
+            
+            // options for the sink defaults in https://github.com/Cimpress-MCP/serilog-sinks-awscloudwatch/blob/master/src/Serilog.Sinks.AwsCloudWatch/CloudWatchSinkOptions.cs
+            var options = new CloudWatchSinkOptions
+            {
+                // you must provide a text formatter, otherwise exception throw
+                TextFormatter = new Serilog.Formatting.Json.JsonFormatter(),
+                // the name of the CloudWatch Log group for logging
+                LogGroupName = logGroupName,
+                
+                // other defaults defaults
+                MinimumLogEventLevel = Serilog.Events.LogEventLevel.Information,
+                BatchSizeLimit = 100,
+                QueueSizeLimit = 10000,
+                Period = TimeSpan.FromSeconds(10),
+                CreateLogGroup = true,
+                LogStreamNameProvider = new DefaultLogStreamProvider(),
+                RetryAttempts = 5
+            };
+            
+            // setup AWS CloudWatch client
+            var client = new AmazonCloudWatchLogsClient("yourAwsAccessKey", "yourAwsSecretAccessKey", RegionEndpoint.USWest2);
+
+            lscf.WriteTo
+                .AmazonCloudWatch(options, client);
         }
     }
 }
